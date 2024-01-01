4824609db9c5b32b9170e850a2a599c4357147a7
sowmik-sec <sowmik.sec@gmail.com>
2024-01-01 22:15:32 +0600

    var, let and constant in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 506;
+let accountEmail = "email@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kurigram";
+
+//accountId = 2;
+accountEmail = "abc@gmail.com";
+accountPassword = "12122121232";
+accountCity = "Dallas";
+
+// console.log(accountId);
+// console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
