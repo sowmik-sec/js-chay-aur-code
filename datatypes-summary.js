733f6e78a36675be2a7c9ef1d99036e4f43c3b9c
sowmik-sec <sowmik.sec@gmail.com>
2024-01-02 07:19:46 +0600

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail; // undefined
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+const bigNumber = 4124323769516237463782652387462864263482634782342n;
+// console.log(bigNumber);
+
+// Reference type(Non primitive)
+// Array, Objects, Functions
+
+const heros = ["Thor", "Wanda", "Doctor Strange"];
+
+let myObj = {
+  name: "Sowmik",
+  age: 28,
+};
+
+const myFunction = function () {
+  console.log("Hello");
+};
+
+console.log(typeof bigNumber);
