a0776d29a4c1f965b3e73dd7b7e8f85a13174af2
sowmik-sec <sowmik.sec@gmail.com>
2024-01-02 06:50:51 +0600

    string to number conversion(confusing)

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc";
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -14,9 +14,26 @@ console.log(valueInNumber);
 let isLoggedIn = "0";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// ****************** Operations *******************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " Sowmik";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+console.log(true);
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
