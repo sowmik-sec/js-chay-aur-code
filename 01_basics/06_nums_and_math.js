9c3ca9794cf4961b3fc6d4fcf6c87306490521d1
sowmik-sec <sowmik.sec@gmail.com>
2024-01-02 09:20:11 +0600

    maths and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,28 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.324345;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// *************************************************
+
+// console.log(Math);
+// console.log(Math.abs(-423));
+// console.log(Math.round(5.2));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(3.9));
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
