7a9be767280991f32a0578dba955e46468fe2b2a
sowmik-sec <sowmik.sec@gmail.com>
2024-01-03 10:40:10 +0600

    while and do while loop in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+let index = 0;
+while (index <= 10) {
+  //   console.log(`Value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["flash", "batman", "superman"];
+index = 0;
+// while (index < myArray.length) {
+//   console.log(`Value is ${myArray[index++]}`);
+// }
+
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
