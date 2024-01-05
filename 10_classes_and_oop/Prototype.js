95df5c207cca4051ba40661fe7057644b98ed0f1
sowmik-sec <sowmik.sec@gmail.com>
2024-01-05 21:51:53 +0600

    magic of prototype in js

--- 10_classes_and_oop/Prototype.js
+++ 10_classes_and_oop/Prototype.js
@@ -44,3 +44,17 @@ const TASupport = {
 };
 
 Teacher.__proto__ = User;
+
+//modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher);
+
+let anotherUsername = "Shohidul      ";
+
+String.prototype.trueLength = function () {
+  console.log(`${this}`);
+  //   console.log(`${this.name}`);
+  console.log(`True length is ${this.trim().length}`);
+};
+
+anotherUsername.trueLength();
+"ice cream".trueLength();
